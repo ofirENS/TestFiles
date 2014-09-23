f136eb9d7b7ea8bb9750626503af46c5ea6ead7e
ofirENS <ofirshukron@gmail.com>
2014-09-24 00:46:59 +0200

    changed the goal function

--- code/OptimizeEncounterDistances.m
+++ code/OptimizeEncounterDistances.m
@@ -1,13 +1,12 @@
-function opimtDists = OptimizeEncounterDistances(observedProb)
+function optimDists = OptimizeEncounterDistances(observedProb)
 % assuming encounter frequencies follow a*d^(-1.5) model 
 % and given the observed encounter probabilities, find the distances which
 % optimize the error between the model and observed data 
-
+close all
 numObs = numel(observedProb);% umber of observations 
-optimdists = fminsearch(@MyFun,[1:numObs]);
+beta   = 1.41;
+MyFun  = @(x) sum((((x.^(-beta))-sum(x.^(-beta)).*observedProb).^2)./sum(x.^(-beta))^2);
+[optimDists,fVal] = fminsearch(MyFun,[1:numObs],optimset('TolFun',1e-16,'TolX',1e-16,'MaxIter',1e6));
 
-end
-
-function valsout = MuFun(x)
-
+figure, plot(1:numObs,optimDists)
 end
