8af01f5d35a34244227f9b25e79ce39b1db88592
ofirENS <ofirshukron@gmail.com>
2014-09-26 00:47:46 +0200

    s

--- code/OptimizeEncounterDistances.m
+++ code/OptimizeEncounterDistances.m
@@ -4,7 +4,7 @@ function optimDists = OptimizeEncounterDistances(observedProb)
 % optimize the error between the model and observed data 
 close all
 numObs = numel(observedProb);% umber of observations 
-beta   = 1.41;
+beta   = 1.05;
 MyFun  = @(x) sum((((x.^(-beta))-sum(x.^(-beta)).*observedProb).^2)./sum(x.^(-beta))^2);
 [optimDists,fVal] = fminsearch(MyFun,[1:numObs],optimset('TolFun',1e-16,'TolX',1e-16,'MaxIter',1e6));
 
