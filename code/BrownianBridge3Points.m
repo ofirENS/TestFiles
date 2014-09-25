c1cb0be24e8afaec115ef0ca0d42c76ef6bed253
ofirENS <ofirshukron@gmail.com>
2014-09-26 01:21:08 +0200

    changed the transformation of the brownian bridge with 3 points. It still needs work, since there seem to be translation and rotation with the current transformation and not folding of the path.

--- code/BrownianBridge3Points.m
+++ code/BrownianBridge3Points.m
@@ -7,16 +7,23 @@ for pIdx = 2:100
 end
 tx = 1; % first time 
 x  = p(tx,:); % first point 
-ty = 10;% second time 
-y  = p(ty,:)+rand(1,3);% second point
-tk = 40;% third time 
-k  = p(tk,:)+randn(1,3);% third point
+ty = 20;% second time 
+y  = p(ty,:)+3*randn(1,3);% second point
+tk = 80;% third time 
+k  = p(tk,:)+10*randn(1,3);% third point
 
-beta  = k-(tk/(2*ty))*(p(ty,:)-y+x)-0.5*(p(tk,:)+x);
-alpha = (ty/tk)*(p(tk,:)+x-k+beta)-y; 
-w= zeros(size(p));
+% beta  = k-(tk/(2*ty))*(p(ty,:)-y+x)-0.5*(p(tk,:)+x);
+alpha = -k -0.5*(x-y+(ty/tk)*(p(tk,:)+x-k)+p(tk,:));
+beta  = -y -(tk/ty)*(p(ty,:)+x+k-y+alpha);
+w     = zeros(size(p));
+
 for wIdx = 1:N
-    w(wIdx,:) = x+p(wIdx,:)-(wIdx/ty)*(p(ty,:)+x-y+alpha)-(wIdx/tk)*(p(tk,:)+x-k+beta);
+    w(wIdx,:) = x+p(wIdx,:)-(wIdx/ty)*(p(ty,:)+x-y+k+alpha)-(wIdx/tk)*(p(tk,:)+x-k+y+beta);
 end
 figure, plot3(p(:,1),p(:,2),p(:,3),'b'), hold on, plot3(w(:,1),w(:,2),w(:,3),'r')
+% draw the lines between points 
+plot3([p(ty,1),w(ty,1)],[p(ty,2),w(ty,2)],[p(ty,3),w(ty,3)],'g','LineWidth',3)
+plot3([p(tk,1),w(tk,1)],[p(tk,2),w(tk,2)],[p(tk,3),w(tk,3)],'g','LineWidth',3)
+daspect([1 1 1]), axis vis3d
+sum(sum((w-p).^2))
 end
