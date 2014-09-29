a84cc12c978c85348f2b90baef6b0bdfe66ee422
ofirENS <ofirshukron@gmail.com>
2014-09-29 21:15:40 +0200

    changed the 3 point bridge (no success, it cannot be done with a linear transformation)

--- code/BrownianBridge3Points.m
+++ code/BrownianBridge3Points.m
@@ -8,22 +8,27 @@ end
 tx = 1; % first time 
 x  = p(tx,:); % first point 
 ty = 20;% second time 
-y  = p(ty,:)+3*randn(1,3);% second point
+y  = p(ty,:)+randn(1,3);% second point
 tk = 80;% third time 
-k  = p(tk,:)+10*randn(1,3);% third point
+k  = p(tk,:)+randn(1,3);% third point
 
 % beta  = k-(tk/(2*ty))*(p(ty,:)-y+x)-0.5*(p(tk,:)+x);
-alpha = -k -0.5*(x-y+(ty/tk)*(p(tk,:)+x-k)+p(tk,:));
-beta  = -y -(tk/ty)*(p(ty,:)+x+k-y+alpha);
+% alpha = -k -0.5*(x-y+(ty/tk)*(p(tk,:)+x-k)+p(tk,:));
+beta  = ((x-k+p(tk,:))/tk -(x-y+p(ty,:))/ty)/2;
+alpha = (x-y+p(ty,:))/ty - beta;
 w     = zeros(size(p));
 
-for wIdx = 1:N
-    w(wIdx,:) = x+p(wIdx,:)-(wIdx/ty)*(p(ty,:)+x-y+k+alpha)-(wIdx/tk)*(p(tk,:)+x-k+y+beta);
+for t = 1:N
+    w(t,:) = x+p(t,:)-t*alpha-t*beta;
 end
-figure, plot3(p(:,1),p(:,2),p(:,3),'b'), hold on, plot3(w(:,1),w(:,2),w(:,3),'r')
+figure, plot3(p(:,1),p(:,2),p(:,3),'b'), hold on,
+        plot3(w(:,1),w(:,2),w(:,3),'r'); % plot the transformed path
 % draw the lines between points 
 plot3([p(ty,1),w(ty,1)],[p(ty,2),w(ty,2)],[p(ty,3),w(ty,3)],'g','LineWidth',3)
 plot3([p(tk,1),w(tk,1)],[p(tk,2),w(tk,2)],[p(tk,3),w(tk,3)],'g','LineWidth',3)
+plot3(x(1),x(2),x(3),'ob');
+plot3(y(1),y(2),y(3),'or');
+plot3(k(1),k(2),k(3),'og');
 daspect([1 1 1]), axis vis3d
-sum(sum((w-p).^2))
+cameratoolbar
 end
