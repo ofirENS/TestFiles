ea548a6c45ca70e1562a5e24739c17dc09292654
ofirENS <ofirshukron@gmail.com>
2014-12-06 01:05:50 +0100

    confilict edited

--- code/scrTestPeakCalling.m
+++ code/scrTestPeakCalling.m
@@ -4,7 +4,7 @@ addpath(genpath(fullfile(pwd,'..','..','Utils')))
 % set seed 
 s          = rng(6);
 numSignals = 50;
-noiseStd   = 0.5; 
+noiseStd   = 0.1; 
 k = zeros(numSignals);
 close all 
 for kIdx = 1:numSignals
@@ -13,23 +13,12 @@ for kIdx = 1:numSignals
 %     if m<0
 %         k(kIdx,:) = k(kIdx,:)-m;
 %     end
-%     k(kIdx,:) = noiseStd*rand(1,numSignals);
-    k(kIdx,:) = sin(linspace(-pi,pi,numSignals)) +noiseStd*randn(1,numSignals)+1;
+    k(kIdx,:) = noiseStd*rand(1,numSignals);
+%     k(kIdx,:) = sin(linspace(-pi,pi,numSignals)) +noiseStd*randn(1,numSignals)+1;
     k(kIdx,:) = k(kIdx,:)./sum(k(kIdx,:));
 end
 
 % Add peaks 
-<<<<<<< HEAD
- k(10,10)  = k(10,10)*2;
- k(10,:)   = k(10,:)./sum(k(10,:));
- k(20,20)  = k(20,20)*2;
- k(20,:)   = k(20,:)./sum(k(20,:));
- k(30,30)  = k(30,30)*2;
- k(30,:)   = k(30,:)./sum(k(30,:));
- k(40,40)  = k(40,40)*2; 
- k(40,:)   = k(40,:)./sum(k(40,:));
- k(50,50)  = k(50,50)*2; 
-=======
  k(10,10)  = abs(k(10,10))*3;
  k(10,:)   = k(10,:)./sum(k(10,:));
  k(20,20)  = abs(k(20,20))*3;
@@ -39,7 +28,6 @@ end
  k(40,40)  = abs(k(40,40))*3; 
  k(40,:)   = k(40,:)./sum(k(40,:));
  k(50,50)  = abs(k(50,50))*3; 
->>>>>>> 2b1c2febb61af08ea62f5ff20a69bbf84deb2287
  k(50,:)   = k(50,:)./sum(k(50,:));
  
 p                       = PeakCalling();
