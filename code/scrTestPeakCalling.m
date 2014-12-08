083a2ee41bcf141c4567e7cb47bd259ffd331682
ofirENS <ofirshukron@gmail.com>
2014-12-09 00:14:27 +0100

    peak calling changed

--- code/scrTestPeakCalling.m
+++ code/scrTestPeakCalling.m
@@ -1,49 +1,57 @@
 %scr Test PeakCalling
+close all 
 % add utils folder 
 addpath(genpath(fullfile(pwd,'..','..','Utils')))
 % set seed 
-s          = rng(14);
-numSignals = 50;
-noiseStd   = 0.1; 
-k = zeros(numSignals);
-close all 
-for kIdx = 1:numSignals
-    k(kIdx,:) = 1*(1:numSignals).^(-1.5)+ noiseStd*randn(1,numSignals);
-    m         = (min(k(kIdx,:)));
-    if m<0
-        k(kIdx,:) = k(kIdx,:)-m;
-    end
-%     k(kIdx,:) = noiseStd*rand(1,numSignals);
-%     k(kIdx,:) = sin(linspace(-pi,pi,numSignals)) +noiseStd*randn(1,numSignals)+1;
-    k(kIdx,:) = k(kIdx,:)./sum(k(kIdx,:));
-end
+s          = rng(15); % randomm seed
 
-% Add peaks 
- k(10,10)  = abs(k(10,10))*3;
- k(10,:)   = k(10,:)./sum(k(10,:));
- k(20,20)  = abs(k(20,20))*3;
- k(20,:)   = k(20,:)./sum(k(20,:));
- k(30,30)  = abs(k(30,30))*3;
- k(30,:)   = k(30,:)./sum(k(30,:));
- k(40,40)  = abs(k(40,40))*3; 
- k(40,:)   = k(40,:)./sum(k(40,:));
- k(50,50)  = abs(k(50,50))*3; 
- k(50,:)   = k(50,:)./sum(k(50,:));
+% construct synthetic example
+numSignals = 100;
+noiseStd   = 0.1; 
+c          = 0.7; % peak magnification factor
+p          = 0.01; % Bernoulli prob.
+k          = zeros(numSignals);
+for kIdx = 1:numSignals
+    noiseTerm = (1+c*binornd(1,p,[1,numSignals]));
+    
+%     %----- ex. 01 encounter by Rouse-----------
+%     k(kIdx,:) = 1*(1:numSignals).^(-1.5)+ noiseStd*randn(1,numSignals).*noiseTerm;
+%     m         = (min(k(kIdx,:)));
+%     if m<0
+%         k(kIdx,:) = k(kIdx,:)-m;
+%     end
+%     k(kIdx,:) = k(kIdx,:)./sum(k(kIdx,:));
+
+
+%    % ---- ex. 02 random signal ------------
+%     k(kIdx,:) = noiseStd*rand(1,numSignals).*noiseTerm;
+%     k(kIdx,:) = k(kIdx,:)./sum(k(kIdx,:));
+
+%    %---- ex. 03  sine wave-----
+%     k(kIdx,:) = sin(linspace(-pi,pi,numSignals)) +noiseStd.*randn(1,numSignals).*(noiseTerm);
+  
+end
  
- load(fullfile(pwd,'..','..','PolymerChainDynamics','Code','ExperimentDataAnalysis','savedAnalysisTADDAndE.mat'))
+ % load encounter signal from data 
+ cName = getenv('computername');
+ switch lower(cName)
+     case 'ofir-hp'
+      load(fullfile(pwd,'..','..','Polymer Chain Dynamics','Code','ExperimentDataAnalysis','savedAnalysisTADDAndE.mat'))
+ end
+%  
  beadRange.bead1 = 108:307;
  beadRange.bead2 = 108:307;
  [~,~,k] = a.ProcessEncounters(beadRange,'Average');
-p                       = PeakCalling();
+ p                       = PeakCalling();
 % Set parameters 
-p.params.fitType        = 'mean';
-p.params.peaksDirection = 'high';
-p.params.smoothingSpan  = ceil(size(k,2)/10);
-p.params.rejectionThresh = 0.99;
-p.params.rejectionTNew   = 0.99;
-p.params.checkPeakForLocalMaximality = false;
+ p.params.fitType        = 'mean';
+ p.params.peaksDirection = 'high';
+ p.params.smoothingSpan  = ceil(size(k,2)/10);
+ p.params.rejectionThresh = 0.99;
+ p.params.rejectionTNew   = 0.99;
+ p.params.checkPeakForLocalMaximality = false;
 
-p.FindPeaks(k);
-p.DisplayPeaks
-p.peakList
+ p.FindPeaks(k);
+ p.DisplayPeaks
+ p.peakList
 % p.DisplaySignalDistributions('cdf')
