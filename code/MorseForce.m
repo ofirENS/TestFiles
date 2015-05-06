927101c6941e976ff64dc3bd1dc565be1956b993
ofirENS <oshukron@biologie.ens.fr>
2015-05-06 16:33:17 +0200

    test the Morse function before making a .mex out of it. The function was written to support the mex format expected

--- code/MorseForce.m
+++ code/MorseForce.m
@@ -1,23 +1,66 @@
-function [force] = MorseForce(De,alpha, re, edgesVector, particleDistance)
+function [force] = MorseForce(De,alpha, re, edgesX,edgesY,edgesZ, particleDistance,forceType)
 % calculate the Morse force, derived from the Morse potential
 % u = De*(exp(-2*a(r-re)) -2*exp(-a*(r-re)))
-% and the force 
+% The force is given by 
 % F = dv/dr = De*(-2*a*exp(-2*a*(r-re)) +2*a*exp(-a*(r-re)))
+% Input parameters:
+% re - the equilibrium bond distance 
+% De - the well depth
+% alpha - the width of the potential
+% edgesX, edgesY, edgesZ -  are numPartXnumPart matrices representing the vectors
+% between particles in the X YT and Z respectively.
+% particleDistance -  a numPartXnumPart matrix of pairwise distances 
+% forceType is a string [attractive |repuslsive |full] indicating whether
+% to use the attractive repulsive part of the Morse force or both
+% For forces in 1 or 2D set the position of the particle in 2-3 dimension
+% to be constant (preferably 0) prior to using this function 
 
-% re is the equilibrium bond distance 
-% De is the well depth
-% alpha is the width of the potential
-tic
-% dim   = size(edgesVector,3);
-force = De*(-2*alpha*exp(-2*alpha*(particleDistance -re)) +2*alpha*exp(-alpha*(particleDistance-re)));
-force = sum(force,2);
-% normalize the mean direction vectors 
-numParticles = size(edgesVector,2);
-dim          = size(edgesVector,3);
-dirVec       = zeros(numParticles,dim);
-for dIdx = 1:dim
-    dirVec(:,dIdx) = sqrt(sum(edgesVector(:,:,dIdx),2).^2);
+% edgesVector(:,:,1)  = edgesX;
+% edgesVector(:,:,2)  = edgesY;
+% edgesVector(:,:,3)  = edgesZ;
+
+dim = 3; 
+% dim                 = size(edgesVector,3);
+numParticles        = size(particleDistance,1);
+
+% Calculate the force magnitude
+repulse      = exp(-alpha*(particleDistance-re)); % repulsive term
+attract      = 1-repulse; % attractive term 
+if strcmpi(forceType,'attractive')
+    forceMag = 2*alpha*De*attract;
+elseif strcmpi(forceType,'repulsive')
+    forceMag = 2*alpha*De*repulse;
+elseif strcmpi(forceType,'full');
+    forceMag = 2*De*alpha*attract*repulse;
+else
+    error('forceType not supported')
 end
-dirVec = bsxfun(@rdivide, dirVec,sqrt(sum(dirVec.^2,2)));
-force  = bsxfun(@times,force,dirVec);
-toc
+
+
+% remove the diagonal 
+% Normalize the mean direction vectors to unit vectors
+dirNorm = sqrt(edgesX.^2+edgesY.^2+edgesZ.^2);
+dirNorm = dirNorm+eye(numParticles);% put ones in the diagonal to prevent dividing by zeros
+dirVecX = edgesX./dirNorm;
+dirVecY = edgesY./dirNorm;
+dirVecZ = edgesZ./dirNorm;
+
+% dirVec  = bsxfun(@rdivide, edgesVector,dirNorm);
+
+% Multiply by the force magnitude
+forceVecX  = forceMag.*dirVecX;
+forceVecY  = forceMag.*dirVecY;
+forceVecZ  = forceMag.*dirVecZ;
+
+% bsxfun(@times,forceMag,dirVec);
+
+% Calculate the mean force direction and assign it to each particle
+force      = zeros(numParticles,dim);
+force(:,1) = -sum(forceVecX,2);
+force(:,2) = -sum(forceVecY,2);
+force(:,3) = -sum(forceVecZ,2);
+
+
+% for dIdx = 1:dim
+%  force(:,dIdx) = -sum(forceVec(:,:,dIdx),2);
+% end
--- code/scrTestMorseForce.m
+++ code/scrTestMorseForce.m
@@ -1,28 +1,32 @@
 % scrTestMorseForce
 % this script test the Morse force between particles 
 close all 
-
-
-
-numPart = 10;
-cMat    = true(numPart); 
-dt      = 0.1;
-De      = 1;   % De is the well depth
-alpha   = 2;   % alpha is the width of the potential
-re      = 0.00; % re is the equilibrium bond distance 
-diffusionConst = 0.001;
-dimension = 3;
+profile on 
+numPart = 100;
+cMat    = true(numPart); % conectivity matrix
+dt      = 0.1; % time step
+De      = .01;   % De is the well depth
+alpha   = .01;   % alpha is the width of the potential
+re      =  2; % re is the equilibrium bond distance 
+forceType      = 'full'; % [attractive|repulsive|full]
+diffusionConst = 1;
+dimension      = 3;
 % initial position 
 r       = rand(numPart,dimension);
 % add random noise 
 f  = figure;
-a  = axes('Parent',f,'NextPlot','replaceChildren','XLim',[-5 5],'YLim',[-5 5],'ZLim',[-5 5]);
+a  = axes('Parent',f,'NextPlot','replaceChildren','XLim',[-15 15],'YLim',[-15 15],'ZLim',[-15 15]);
 l  = line('XData',r(:,1), 'YData',r(:,2),'ZData',r(:,3),'Marker','o','LineStyle','none','Parent',a);
 cameratoolbar
-for step = 1:500; 
-    particleDist = ForceManager.GetParticleDistance(r); 
+for step = 1:100; 
+    particleDist = ForceManager.GetParticleDistance(r);
     edgesVec     = ForceManager.GetEdgesVectors(r,cMat);
-    r            = r+MorseForce(De,alpha,re,edgesVec,particleDist)*dt +sqrt(2*diffusionConst*dt)*randn(numPart,dimension);
+    tic
+    mForce       = MorseForce(De,alpha,re,edgesVec(:,:,1),edgesVec(:,:,2), edgesVec(:,:,3),particleDist,forceType);
+    toc
+    r            = r+mForce*dt ;%+sqrt(2*diffusionConst*dt)*randn(numPart,dimension);
     set(l,'XData',r(:,1),'YData',r(:,2),'ZData',r(:,3));
     drawnow;
 end
+profile viewer
+
