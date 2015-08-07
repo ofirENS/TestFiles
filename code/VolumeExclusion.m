d43d32b158efd8b4f582cf70d5e87f787dd3feb7
ofirENS <oshukron@biologie.ens.fr>
2015-08-07 21:30:47 +0200

    changed some simulation setting

--- code/VolumeExclusion.m
+++ code/VolumeExclusion.m
@@ -1,22 +1,27 @@
 function VolumeExclusion
 close all 
 % construct the framework 
-simulatorParams = SimulationFrameworkParams('dt',0.1,'dimension',3,'numSteps',1,...
+simulatorParams = SimulationFrameworkParams('dt',0.01,'dimension',3,'numSteps',1,...
     'objectInteraction',false,'showSimulation',true);
     
-domainForces = ForceManagerParams('diffusionForce',false,'diffusionConst',1,'mechanicalForce',true,...
-    'mechanicalForceCenter',[0 0 0],'mechanicalForceDirection','out','mechanicalForceCutoff',1,'mechanicalForceMagnitude',0.1);
-dp            = DomainHandlerParams('domainShape','open','dimension',simulatorParams.simulator.dimension,...
+domainForces = ForceManagerParams('diffusionForce',true,'diffusionConst',0.1,'mechanicalForce',false);%
+    
+dp            = DomainHandlerParams('domainShape','sphere','domainWidth',1,'dimension',simulatorParams.simulator.dimension,...
                                     'forceParams',domainForces);
-chainForces = ForceManagerParams('minParticleEqDistance',0,'springForce',true,'springConst',0,'bendingElasticityForce',false);
+chainForces = ForceManagerParams('minParticleEqDistance',0,'springForce',true,'springConst',1,'bendingElasticityForce',false,...
+                                 'mechanicalForce',true,'mechanicalForceCenter',[0 0 0],'mechanicalForceDirection','out',...
+                                 'mechanicalForceCutoff',1,'mechanicalForceMagnitude',1);
+                             
 cp          = ChainParams('forceParams',chainForces,'numBeads',100,'b',1,'dimension',simulatorParams.simulator.dimension);
 simulatorParams.SetDomainParams(dp);
 simulatorParams.SetChainParams(cp);
 r    = RouseSimulatorFramework(simulatorParams);
 r.Run
-for sIdx = 1:1000
-   r.handles.classes.domain.params.forceParams.mechanicalForceCenter = r.objectManager.curPos;
+for sIdx = 1:200
+    r.objectManager.handles.chain.params.forceParams.mechanicalForceCenter = r.objectManager.handles.chain.position.cur;
     r.Step    
-    
+%     f = ForceManager.GetParticleDistance(r.objectManager.curPos);
+%     f(1:cp.numBeads+1:cp.numBeads^2)=Inf; 
+%     k = find(f<chainForces.mechanicalForceCutoff)
 end
 end
