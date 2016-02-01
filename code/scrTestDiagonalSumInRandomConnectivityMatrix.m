3dd08de7f1a4c85b3cebec46ca44ba57619ae7e3
ofirENS <ofirshukron@gmail.com>
2016-02-01 10:03:22 +0100

    test code for the diagonal in a random matrix

---
+++ code/scrTestDiagonalSumInRandomConnectivityMatrix.m
@@ -0,0 +1,39 @@
+% scr test the probability of sums in the diagonal of random matrices with
+% alpha connectivity level.
+% This experiment is done to test the expected diagonal value in a Rouse
+% matrix with extra connectors 
+clear all 
+close all 
+N              = 30; % num cells
+m              = 12;  % expected sum
+alpha          = 0.7;
+numAlphas      = 50;
+numExperiments = 100;
+s = zeros(N-1,numAlphas);
+for alphaIdx = 1:numAlphas
+ numConnectors = floor(0.01*alphaIdx*(N-1)*(N/2-1));
+ diagSum = zeros(N,numExperiments);
+% choose random cells in the upper triangular matrix removing the super
+% diagonal
+
+for expIdx =1:numExperiments
+    z  = zeros(1,(N-1)*(N/2-1));
+    rp = randperm((N-1)*(N/2 -1));
+    rp = rp(1:numConnectors);
+    z(rp) = 1;
+    % translate the choices into the matrix
+    mat = zeros(N);
+    for mIdx = 1:N-2
+        mat(mIdx,mIdx+2:end) =z(1:N-mIdx-1);
+        z=z(N-mIdx:end);
+    end
+    mat               = (mat+mat');
+    diagSum(:,expIdx) = sum(mat,2);
+end
+
+% calculate the fraction of times the sum equals m
+
+for mIdx = 1:N-1
+s(mIdx,alphaIdx) =sum(diagSum(:)==mIdx)/(N*numExperiments);
+end
+end
