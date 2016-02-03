4c929be5e061c7573c33a89ac3d79aebf0132fdc
ofirENS <ofirshukron@gmail.com>
2016-02-04 00:44:13 +0100

    the eigenvalues are now correctly extracted

--- code/scrTestDiagonalSumInRandomConnectivityMatrix.m
+++ code/scrTestDiagonalSumInRandomConnectivityMatrix.m
@@ -9,26 +9,30 @@ N              = 50; % num cells
 m              = 12;  % expected sum
 alpha          = 0.1;
 numAlphas      = 10;
-numExperiments = 100;
+numExperiments = 200;
 s              = zeros(N-1,numAlphas);
 sMat           = zeros(N,N,numAlphas);
 gs             = zeros(N-1,numAlphas);
 meanGs         = zeros(1,numAlphas);
 meanGsTheoretical = zeros(1,numAlphas);
 meanGsTheoreticalEndMonomers = zeros(1,numAlphas);
+Nl             = (N-2)*(N-1)/2;
 B              = zeros(N,N,numAlphas);
 % Theoretical distribution of the sum of the diagonal entries 
-diagEqn =  @(N,nc,n) nchoosek((N-2)*(N-1)/2 -(N-3),nc-n).*nchoosek(N-3,n)./nchoosek((N-2)*(N-1)/2, nc);
+diagEqn =  @(N,nc,n) nchoosek(Nl -(N-3),nc-n).*nchoosek(N-3,n)./nchoosek(Nl, nc);
+% Get Rouse matrix with eigenvalues and vectors (m file in test files) 
+[rouseMat, rouseEigenVec, rouseEigenVal] = RouseMatrixSimple(N);
+
 for alphaIdx = 1:numAlphas
     
- numConnectors = floor(0.01*alphaIdx*(N-1)*(N/2-1));
+ numConnectors = floor(0.01*alphaIdx*Nl);
  diagSum       = zeros(N,numExperiments);
 % choose random cells in the upper triangular matrix removing the super
 % diagonal
 
 for expIdx = 1:numExperiments
-    z     = zeros(1,(N-1)*(N/2-1));
-    rp    = randperm((N-1)*(N/2 -1));
+    z     = zeros(1,Nl);%(N-1)*(N/2-1));
+    rp    = randperm(Nl);%(N-1)*(N/2 -1));
     rp    = rp(1:numConnectors);
     z(rp) = 1;
     % translate the choices into the matrix
@@ -56,8 +60,8 @@ for mIdx = 1:N-3
     
 end
 meanGs(alphaIdx) = trapz(1:size(gs,1),(1:size(gs,1)).*gs(:,alphaIdx)');
-meanGsTheoretical(alphaIdx)            = (N-3)*numConnectors/((N-2)*(N-1)/2);
-meanGsTheoreticalEndMonomers(alphaIdx) = (N-2)*numConnectors/((N-2)*(N-1)/2);
+meanGsTheoretical(alphaIdx)            = (N-3)*numConnectors/Nl;
+meanGsTheoreticalEndMonomers(alphaIdx) = (N-2)*numConnectors/Nl;
 
 % construct the average matrix from the mean 
 temp = zeros(N);
@@ -71,12 +75,14 @@ temp(:,1)   = -meanGsTheoreticalEndMonomers(alphaIdx)/(N-2);
 temp(:,end) = -meanGsTheoreticalEndMonomers(alphaIdx)/(N-2);
 
 temp(diag(true(1,N-1),1) | diag(true(1,N-1),-1) | diag(true(1,N),0)) = 0;
-temp(diag(true(1,N),0)) = -sum(temp,2);
-B(:,:,alphaIdx)         = temp;
+temp(diag(true(1,N),0)) = meanGsTheoretical(alphaIdx);%-sum(temp,2);
+temp(1,1)       = meanGsTheoreticalEndMonomers(alphaIdx);
+temp(end,end)   = meanGsTheoreticalEndMonomers(alphaIdx);
+B(:,:,alphaIdx) = temp;
+[0 numConnectors/Nl *N +rouseEigenVal(2:end).*(1-numConnectors/Nl)];
 end
 
-% Get Rouse matrix with eigenvalues and vectors (m file in test files) 
-[rouseMat, rouseEigenVec, rouseEigenVal] = RouseMatrixSimple(N);
+
 
 
 figure, plot(s),  title('simulation derived distribution')
